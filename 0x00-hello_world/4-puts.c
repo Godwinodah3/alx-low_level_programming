c22d3efdf60441602ab77a3033ac296c2331527c
odah <davegw57@gmail.com>
2022-09-08 16:10:49 -0700

    using puts to print words

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, Programming
+ *
+ * Return: Always 0 (Success)	
+ */			
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzl");		    return (0);
+}
