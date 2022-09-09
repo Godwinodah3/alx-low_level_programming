cfe6dc32c1edf2beeffba51c3c3a19331a9d64b3
odah <davegw57@gmail.com>
2022-09-08 17:17:45 -0700

    using Puts in printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{   puts("\"Programmming is like build         ing a mutilingual puzzle");
+{   puts("\"Programmming is like            building a multilingual puzzle");
 return (0);
 }
