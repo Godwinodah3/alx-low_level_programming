23844e779013df3a562ed49288dee41437813472
odah <davegw57@gmail.com>
2022-09-10 08:21:46 -0700

    using putchar to print alhabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		putchar(low);
-	}
-	for (low = 'A'; low <= 'Z'; low++)
-	{
-		putchar(low);
-	}
-
+	char a:
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	
 	putchar('\n');
 
 	return (0);
