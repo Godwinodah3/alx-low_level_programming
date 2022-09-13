0a3f9bf843d2dbf835de87edaa81df9ed12e5488
odah <davegw57@gmail.com>
2022-09-13 16:10:04 -0700

    alphabet-10tiems

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,7 +11,8 @@ void print_alphabet(void)
 	char a = a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
+	{	_putchar(a);
+
+		_putchar('\n');
 	}
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - 10times*
+ *Description: Printing alphabets
+ *
+ *Return: Aways 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+
+		int i = 0;
+
+
+		for (i = 0; i <= 10; i++)
+		{
+			_putchar(i);
+		}
+	}
+}
