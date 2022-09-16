20f60e3ff124a7f3cff8606d162edb16d66611f0
odah <davegw57@gmail.com>
2022-09-15 17:08:37 -0700

    two integers multipying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *mul - Multiplying integers recursively using recursion method;
+ *
+ *mul - Function used*
+ *
+ *@a: interger declared
+ *
+ *@b: interger declared
+ *
+ *Return:Integer for calculation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
