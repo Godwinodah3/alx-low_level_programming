726e8b80fcd2b7c8d6cafca0f969a4e9a3aff254
odah <davegw57@gmail.com>
2022-09-19 14:49:59 -0700

    finding length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of the stting
+ *
+ *@s: value used
+ *
+ *Return: type int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (s = 0; s[i] ! = '\0'; i++)
+}
