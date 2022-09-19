d0e2a62dedaf6bf426006de0e50ad7fa4f97e61c
odah <davegw57@gmail.com>
2022-09-19 15:51:58 -0700

    using putchar

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str);
+int main(void)
+
+{
+    char *str;
+    int i;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _putchar(str[i]);
+    return (0);
+}
+
