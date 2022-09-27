7a14d61cf4ab6f838c2503511a4a4c09ee5d7e31
odah <davegw57@gmail.com>
2022-09-27 13:33:24 -0700

    mqin.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@ int _putchar(char c);
 #include <stdio.h>
 #include <string.h>
 char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
