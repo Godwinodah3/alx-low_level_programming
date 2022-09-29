8ae366d6cbb6cc7cc1766ebc98d7474826b25f54
odah <davegw57@gmail.com>
2022-09-29 13:05:30 -0700

    solving factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,9 +14,9 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else if ( n <= 1)
-		{
-			return (1);
-		}
-	return (n * factorial (n - 1));
+	else if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }
